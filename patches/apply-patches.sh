f786b67e6229d784d9ccb42c3ab34813beb8799f
MrFluffyOven <137286588+MrFluffyOven@users.noreply.github.com>
2023-12-27 16:53:02 +0000

    apply nazim patches

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -73,4 +73,37 @@ for path_personal in $(cd $personal; echo *); do
 	popd
 done
 
+printf "\n ### APPLYING NAZIM PATCHES ###\n";
+sleep 1.0;
+for path in $(cd $nazim; echo *); do
+	tree="$(tr _ / <<<$path | sed -e 's;platform/;;g')"
+	printf "\n| $path ###\n";
+	[ "$tree" == build ] && tree=build/make
+    [ "$tree" == vendor/hardware/overlay ] && tree=vendor/hardware_overlay
+    [ "$tree" == treble/app ] && tree=treble_app
+	pushd $tree
+
+	for patch in $nazim/$path/*.patch; do
+		# Check if patch is already applied
+		if patch -f -p1 --dry-run -R < $patch > /dev/null; then
+            printf "### ALREDY APPLIED: $patch \n";
+			continue
+		fi
+
+		if git apply --check $patch; then
+			git am $patch
+		elif patch -f -p1 --dry-run < $patch > /dev/null; then
+			#This will fail
+			git am $patch || true
+			patch -f -p1 < $patch
+			git add -u
+			git am --continue
+		else
+			printf "### FAILED APPLYING: $patch \n"
+		fi
+	done
+
+	popd
+done
+
 rm -rf $1/patches
